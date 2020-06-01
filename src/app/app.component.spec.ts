d5e3b955382f720d0c1b70535e2a43b3920986e8
Karley Petracca <kpetracca@formfree.com>
2020-06-01 11:17:49 -0400

    Changed name - added 'angular'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'api-integration-demo'`, () => {
+  it(`should have as title 'api-integration-demo-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('api-integration-demo');
+    expect(app.title).toEqual('api-integration-demo-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('api-integration-demo app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'api-integration-demo-angular app is running!'
+    );
   });
 });
